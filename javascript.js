daddaf7daa5bb444c13e5668cf4f995be124eacf
peaceout2 <peace2dayshhh@gmail.com>
2025-02-17 10:43:39 +0800

    attempt 1 at building the project

--- javascript.js
+++ javascript.js
@@ -2,14 +2,64 @@ function getComputerChoice(){
 
     const num = Math.floor((Math.random()*3)+ 1);
     if(num === 1){
-        return "Scissor"
+        return "scissor"
     } else if (num === 2) {
-        return "Paper"
+        return "paper"
     } else {
-        return "Rock"
+        return "rock"
     };
 } 
 
-console.log(getComputerChoice())
+function getHumanChoice() {
+
+    let result =prompt("Scissor, Paper or Rock");
+    if(result.toLowerCase() === "scissor") {
+        return "scissor"
+    } else if(result.toLowerCase() === "paper") {
+        return "paper"
+    } else {
+        return "rock"
+    };
+    
+}
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice,computerChoice){
+    if(humanChoice === "scissor" && computerChoice === "paper"){
+        console.log("You win! scissor beat paper")
+        console.log(humanScore += 1)
+    } else if(humanChoice === "paper" && computerChoice === "rock" ){
+        console.log("You win! paper beat rock")
+        console.log(humanScore += 1)
+    } else if(humanChoice ==="rock" && computerChoice === "scissor"){
+        console.log("You win! rock beat scissor")
+        console.log(humanScore +=1)
+    } else if(humanChoice === computerChoice) {
+        console.log("Draw! Nobody wins")
+    } else{
+        console.log(`You lose!${computerChoice} beat ${humanChoice}`)
+        console.log(computerScore += 1)
+    }
+    
+}
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+
+
+function playGame(){
+
+    for(let i=1;i<6;i++) {
+        playRound(getHumanChoice(),getComputerChoice());
+        
+    }
+
+}
+
+playGame();
 
 
